9cc1a393cfd535aa358701c331c6f0901af322bd
enbraining <me@xolving.com>
2023-12-03 15:16:24 +0900

    gong sa zoong

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,16 +6,44 @@ import Metadata from "../../data/siteMetadata";
 export default component$(() => {
   return (
     <>
-      <div class="rounded-icon">
-        <Avatar />
+    <div class='boards'>
+      <div>
+        <div class="rounded-icon">
+          <Avatar />
+        </div>
+        <div class="description">
+          <h2>{Metadata.author}</h2>
+          <p>{Metadata.description}</p>
+        </div>
+        <div class="description">
+          <h2>공사 중 ⚠️</h2>
+        </div>
       </div>
-      <div class="block">
-        <h2>{Metadata.author}</h2>
-        <p>{Metadata.description}</p>
+
+      <div>
+        <div class="xolving">
+          <a href="https://github.com/xolving"><p>xolving으로 가기</p></a>
+        </div>
+        <div class="description">
+          <h2>진짜 공사중<br/>진짜임</h2>
+        </div>
       </div>
-      <div class="block">
-        <h2>공사 중 ⚠️</h2>
+
+      <div>
+        <div class="description">
+          <h2>공</h2>
+        </div>
+        <div class="description">
+          <h2>사</h2>
+        </div>
+        <div class="description">
+          <h2>중</h2>
+        </div>
+        <div class="description">
+          <h2>⚠️</h2>
+        </div>
       </div>
+    </div>
     </>
   );
 });
