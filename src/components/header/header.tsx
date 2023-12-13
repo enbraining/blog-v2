21bae0c3b74f9beb8b9828763a4a39f7a52347f7
enbraining <me@xolving.com>
2023-12-13 18:31:25 +0900

    update headher;

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,6 +5,7 @@ import styles from "./header.module.css";
 export default component$(() => {
   return (
     <header class={styles.header}>
+      <h1 class={styles.hidden}>hello</h1>
       <div class={styles.logo}>
         <a href="/" title="qwik">
           <Logo height={50} width={50} />
